670c5fd0931562c3831b3b27427ed8308803133b
Dan Russell <dan.russell@cpoms.co.uk>
2021-04-29 10:12:06 +0100

    Add light/dark modes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_theme
 
   private
 
@@ -8,6 +9,15 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["ADMIN_USERNAME"] && password == ENV["ADMIN_PASSWORD"]
       cookies[:authed] = {:value => "lol", :expires => Time.now + 720000}
+    end
+  end
+
+  def set_theme
+    if params[:theme].present?
+      theme = params[:theme].to_sym
+      # session[:theme] = theme
+      cookies[:theme] = theme
+      redirect_to(request.referrer || root_path)
     end
   end
 end
